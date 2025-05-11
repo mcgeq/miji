2290d642b3d1ce40b4e1648a7a394b6586a5d9be
mcgeq <mcgeq@outlook.com>
2025-05-11 11:47:20 +0800

    feat(http): :art: Add http with dio

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,26 @@
 // File:           main.dart
 // Description:    About Flutter main
 // Create   Date:  2025-03-29 16:27:14
-// Last Modified:  2025-05-11 11:13:46
+// Last Modified:  2025-05-11 11:38:14
 // Modified   By:  mcgeq <mcgeq@outlook.com>
 // -----------------------------------------------------------------------------
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:miji/app/app.dart';
 import 'package:miji/config/environment/env.dart';
 import 'package:miji/config/environment/env_config.dart';
+import 'package:miji/services/api/xhttp.dart';
 import 'package:miji/services/logging/miji_logging.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/.env');
   McgLogger.init(enableFileLogging: true, minLevel: LogLevel.verbose);
   loadEnvironment(EnvironmentType.dev);
+  XHttp.getInstance();
   final storageDirectory = await getApplicationDocumentsDirectory();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: HydratedStorageDirectory(storageDirectory.path),
