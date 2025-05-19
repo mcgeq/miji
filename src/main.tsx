97b1acd779d9d9d1a75dd5270363fdf87bf849c7
mcgeq <mcgeq@outlook.com>
2025-05-19 20:47:36 +0800

    feat(config): :wrench: Add unocss reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
 import 'virtual:unocss-devtools';
 
