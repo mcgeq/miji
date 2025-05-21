43da09991658fd82f1dd5b8922493c94ca84af93
mcgeq <mcgeq@outlook.com>
2025-05-21 20:57:48 +0800

    feat(nav): :art: Modify nav item

--- src/components/layout/navItems.ts
+++ src/components/layout/navItems.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { path: '/todo', label: '首页', icon: 'i-tabler-home' },
-  { path: '/period', label: '周期', icon: 'i-tabler-calendar' },
+  { path: '/period', label: '经期', icon: 'i-tabler-calendar' },
   { path: '/add', label: '', icon: 'i-tabler-plus', isCenter: true },
   { path: '/checklist', label: '清单', icon: 'i-tabler-list' },
   { path: '/profile', label: '我的', icon: 'i-tabler-user' },
