ff9ff1b5032325a48870935c4e2702d5c5883129
mcgeq <mcgeq@outlook.com>
2025-07-13 10:29:51 +0800

    :sparkles: feature(frontend): Add home split view

--- src/features/home/views/HomeView.vue
+++ src/features/home/views/HomeView.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>Home</div>
+  <div class="grid grid-cols-1 md:grid-cols-2 grid-rows-4 md:grid-rows-2 gap-4 p-2 min-h-screen bg-gray-100">
+    <div class="home-scoop text-red-600">
+      区域 1
+    </div>
+    <div class="home-scoop text-blue-600">
+      区域 2
+    </div>
+    <div class="home-scoop text-purple-600">
+      区域 3
+    </div>
+    <div class="home-scoop text-yellow-600">
+      区域 4
+    </div>
+  </div>
 </template>
 
 <script setup>
-  
 </script>
+
+<style lang="postcss">
+  .home-scoop {
+    @apply bg-white rounded-lg shadow-md flex items-center justify-center text-xl font-semibold;
+  }
+</style>
