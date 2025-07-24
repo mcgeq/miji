7a19bf8f91be891ce15cfdca0a501d5847b58501
mcgeq <mcgeq@outlook.com>
2025-07-24 19:08:53 +0800

    :recycle: refactor(frontend): Refactor date utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -228,11 +228,7 @@ export class DateUtils {
    * @returns 格式化后的日期字符串
    */
   static formatDate(dateStr: string) {
-    const date = new Date(dateStr);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从0开始
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    return DateUtils.formatDatePart(new Date(dateStr));
   }
 
   /**
