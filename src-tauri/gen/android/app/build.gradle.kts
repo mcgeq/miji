8e4e4dddcf5a530523ee6a39375ab1f465f48eca
mcgeq <mcgeq@outlook.com>
2025-09-20 09:06:29 +0800

    :robot: cli(android): Add cli

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -1,57 +1,85 @@
 import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("rust")
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+  id("rust")
 }
 
-val tauriProperties = Properties().apply {
-    val propFile = file("tauri.properties")
-    if (propFile.exists()) {
-        propFile.inputStream().use { load(it) }
-    }
-}
+val tauriProperties =
+        Properties().apply {
+          val propFile = file("tauri.properties")
+          if (propFile.exists()) {
+            propFile.inputStream().use { load(it) }
+          }
+        }
 
 android {
-    compileSdk = 34
-    namespace = "com.mcgeq"
-    defaultConfig {
-        manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.mcgeq"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
-        versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
-    }
-    buildTypes {
-        getByName("debug") {
-            manifestPlaceholders["usesCleartextTraffic"] = "true"
-            isDebuggable = true
-            isJniDebuggable = true
-            isMinifyEnabled = false
-            packaging {                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
-                jniLibs.keepDebugSymbols.add("*/armeabi-v7a/*.so")
-                jniLibs.keepDebugSymbols.add("*/x86/*.so")
-                jniLibs.keepDebugSymbols.add("*/x86_64/*.so")
-            }
+  compileSdk = 34
+  namespace = "com.mcgeq"
+  defaultConfig {
+    manifestPlaceholders["usesCleartextTraffic"] = "false"
+    applicationId = "com.mcgeq"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
+    versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
+  }
+  signingConfigs {
+    create("release") {
+        val keystorePropertiesFile = rootProject.file("keystore.properties")
+        val keystoreProperties = Properties()
+        if (keystorePropertiesFile.exists()) {
+            keystoreProperties.load(FileInputStream(keystorePropertiesFile))
         }
-        getByName("release") {
-            isMinifyEnabled = true
-            proguardFiles(
-                *fileTree(".") { include("**/*.pro") }
-                    .plus(getDefaultProguardFile("proguard-android-optimize.txt"))
-                    .toList().toTypedArray()
-            )
-        }
+
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["password"] as String
+        storeFile = file(keystoreProperties["storeFile"] as String)
+        storePassword = keystoreProperties["password"] as String
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+  }
+  buildTypes {
+    getByName("debug") {
+      manifestPlaceholders["usesCleartextTraffic"] = "true"
+      isDebuggable = true
+      isJniDebuggable = true
+      isMinifyEnabled = false
+      packaging {
+        jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
+        jniLibs.keepDebugSymbols.add("*/armeabi-v7a/*.so")
+        jniLibs.keepDebugSymbols.add("*/x86/*.so")
+        jniLibs.keepDebugSymbols.add("*/x86_64/*.so")
+      }
     }
-    buildFeatures {
-        buildConfig = true
+    getByName("release") {
+      signingConfig = signingConfigs.getByName("release")
+      isMinifyEnabled = true
+      proguardFiles(
+              *fileTree(".") { include("**/*.pro") }
+                      .plus(getDefaultProguardFile("proguard-android-optimize.txt"))
+                      .toList()
+                      .toTypedArray()
+      )
     }
+  }
+  kotlinOptions { jvmTarget = "1.8" }
+  buildFeatures { buildConfig = true }
 }
+
+rust { rootDirRel = "../../../" }
+
+dependencies {
+  implementation("androidx.webkit:webkit:1.6.1")
+  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("com.google.android.material:material:1.8.0")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.4")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
+
+apply(from = "tauri.build.gradle.kts")
 
 rust {
     rootDirRel = "../../../"
